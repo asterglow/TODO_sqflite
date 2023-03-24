c695c931af52663c064441d7e2a379586318036a
asterglow <cybereincarnate@gmail.com>
2023-03-25 03:18:20 +0530

    Ready for adding to UI

--- lib/services/todo_services.dart
+++ lib/services/todo_services.dart
@@ -1,3 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:todo_sqflite/database/todo_database.dart';
 
-class TodoServices with ChangeNotifier {}
+import '../models/todo_model.dart';
+
+class TodoServices with ChangeNotifier {
+  List<Todo> _todos = [];
+
+  List<Todo> get todos => _todos;
+
+  Future<String> getTodos(String username) async {
+    try {
+      _todos = await TodoDatabase.instance.getAllTodos(username);
+      notifyListeners();
+    } catch (e) {
+      return e.toString();
+    }
+    return 'OK';
+  }
+
+  Future<String> deleteTodo(Todo todo) async {
+    try {
+      await TodoDatabase.instance.deleteTodo(todo);
+    } catch (e) {
+      return e.toString();
+    }
+    String result = await getTodos(todo.username);
+    return result;
+  }
+
+  Future<String> createTodo(Todo todo) async {
+    try {
+      await TodoDatabase.instance.createTodo(todo);
+    } catch (e) {
+      return e.toString();
+    }
+    String result = await getTodos(todo.username);
+    return result;
+  }
+
+  Future<String> toggleTodoDone(Todo todo) async {
+    try {
+      await TodoDatabase.instance.toggleTodoDone(todo);
+    } catch (e) {
+      return e.toString();
+    }
+    String result = await getTodos(todo.username);
+    return result;
+  }
+  
+}
